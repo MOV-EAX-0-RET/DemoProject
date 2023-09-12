c1be747306b9214ceae4bc25bf545daa93876e08
User1 <example@gmail.com>
2023-09-12 20:21:53 +0300

    chars animation

--- DemoProject/DemoProject.cpp
+++ DemoProject/DemoProject.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <vector>
 #include "cldraw.h"
 
 using namespace std;
 
 int main() {
-    create_console();
+    vector<int> xs;
+    vector<int> ys;
+    vector<char> chars;
 
+    for (int i = 0; i < 100; i++) {
+        xs.push_back(rand() % 120);
+        ys.push_back(rand() % 30);
+        chars.push_back('0' + (rand() % 100));
+    }
+
+    create_console();
     while (true) {
-        console_clear(BACKGROUND_RED);
+        console_clear();
+        for (int i = 0; i < xs.size(); i++) {
+            xs[i] += rand() % 3 - 1;
+            ys[i] += rand() % 3 - 1;
+            console_pixel(xs[i], ys[i], chars[i]);
+        }
         console_flip();
     }
     return 0;

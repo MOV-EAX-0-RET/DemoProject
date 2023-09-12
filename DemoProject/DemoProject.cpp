97048f18934909976d1ab45097c870487326b314
User1 <example@gmail.com>
2023-09-12 20:41:47 +0300

    animation upgrade

--- DemoProject/DemoProject.cpp
+++ DemoProject/DemoProject.cpp
@@ -4,24 +4,32 @@
 
 using namespace std;
 
-int main() {
-    vector<int> xs;
-    vector<int> ys;
-    vector<char> chars;
+struct Fly {
+    int x;
+    int y;
+    char c;
+    int color;
+};
 
-    for (int i = 0; i < 100; i++) {
-        xs.push_back(rand() % 120);
-        ys.push_back(rand() % 30);
-        chars.push_back('0' + (rand() % 100));
+int main() {
+    vector<Fly> flies;
+
+    for (int i = 0; i < 150; i++) {
+        Fly fly;
+        fly.x = rand() % 120;
+        fly.y = rand() % 30;
+        fly.c = '0' + (rand() % 100);
+        fly.color = FOREGROUND_RED + rand() % 64;
+        flies.push_back(fly);
     }
 
     create_console();
     while (true) {
         console_clear();
-        for (int i = 0; i < xs.size(); i++) {
-            xs[i] += rand() % 3 - 1;
-            ys[i] += rand() % 3 - 1;
-            console_pixel(xs[i], ys[i], chars[i]);
+        for (Fly& fly : flies) {
+            fly.x += rand() % 3 - 1;
+            fly.y += rand() % 3 - 1;
+            console_pixel(fly.x, fly.y, fly.c, fly.color);
         }
         console_flip();
     }

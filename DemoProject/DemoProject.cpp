7164b42382a5b204b26b975035b79ca648a2390b
User1 <example@gmail.com>
2023-09-14 20:47:16 +0300

    Button and Menu

--- DemoProject/DemoProject.cpp
+++ DemoProject/DemoProject.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <functional>
 #include "cldraw.h"
 
 using namespace std;
@@ -7,19 +8,66 @@ using namespace std;
 struct Button {
     int x;
     int y;
+    int width;
     string text;
     int color;
+    function<void()> action;
+
+    void draw() {
+        console_rect(x, y, width, 3, ' ', color);
+        console_text(text, x + 1, y + 1,
+            color | FOREGROUND_WHITE);
+    }
 };
 
+struct Menu {
+    vector<Button> buttons;
+    int selected = 0;
+
+    void control() {
+        if (console_just_pressed(VK_DOWN)) {
+            selected++;
+            if (selected >= buttons.size()) {
+                selected = 0;
+            }
+        }
+        if (console_just_pressed(VK_UP)) {
+            selected--;
+            if (selected < 0) {
+                selected = buttons.size() - 1;
+            }
+        }
+
+        if (console_just_pressed(VK_RETURN)) {
+            Button& button = buttons[selected];
+            if (button.action) {
+                button.action();
+            }
+        }
+    }
+};
+
+void action_exit() {
+    exit(0);
+}
+
 int main() {
-    Button button{ 3, 3, "Start", BACKGROUND_RED };
+    Menu menu;
+    menu.buttons.push_back(Button{ 3, 3, 20, "Start", BACKGROUND_RED });
+    menu.buttons.push_back(Button{ 3, 7, 20, "Middle", BACKGROUND_RED });
+    menu.buttons.push_back(Button{3, 11, 20,"Exit", BACKGROUND_GREEN, action_exit });
 
     create_console();
     while (true) {
+        menu.control();
+
         console_clear();
-        console_rect(button.x, button.y, 20, 3, ' ', button.color);
-        console_text(button.text, button.x + 1, button.y + 1, 
-                     button.color | FOREGROUND_WHITE);
+        for (Button& button : menu.buttons) {
+            button.draw();
+        }
+        Button& selectedButton = menu.buttons[menu.selected];
+        console_pixel(selectedButton.x - 2, selectedButton.y+1, '>');
+
         console_flip();
     }
     return 0;
